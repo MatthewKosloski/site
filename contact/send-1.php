3f073e359d6342cd0fc7985ef4350f920caf5575
MatthewKosloski <matthewkosloski@me>
2015-06-17 00:40:41 -0500

    new form validation

--- contact/send-1.php
+++ contact/send-1.php
@@ -2,21 +2,17 @@
 
     //Email Recipient
     /* Create Email Variables */
-    $to = 'hi@mtk.me';
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $age = $_POST['age'];
-    $robot = $_POST['robot'];
+    $to = 'matthewkosloski@me.me';
+    $name    = stripslashes(trim($_POST['name']));
+    $email   = stripslashes(trim($_POST['email']));
+    $subject = stripslashes(trim($_POST['subject']));
+    $message = stripslashes(trim($_POST['message']));
     $ip = $_SERVER['REMOTE_ADDR']; 
     
     /* Create email body for applicant */
     $body = "From: " . $name . "<br />";
     $body .= "Email: " . $email . "<br />";
     $body .= "Subject: " . $subject . "<br />";
-    $body .= "Age: " . $age . "<br />";
-    $body .= "Robot?: " . $robot . "<br />";
     $body .= "Message: " . $message . "<br />";
     $body .= "IP: " . $ip . "<br /><br />";
     
@@ -24,7 +20,4 @@
     $headers = "From: $email\r\n";
     $headers .= "Content-type: text/html\r\n";
     mail($to,$subject,$body,$headers);
-    
-    header("location: ../index.html");
-    
 ?>
---
+++ contact/send.php
@@ -0,0 +1,31 @@
+<?php
+$subjectPrefix = "[Message from matthewkosloski.me]";
+$emailTo = 'matthewkosloski@me.com';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name    = stripslashes(trim($_POST['name']));
+    $email   = stripslashes(trim($_POST['email']));
+    $subject = stripslashes(trim($_POST['subject']));
+    $message = stripslashes(trim($_POST['message']));
+    $ip = $_SERVER['REMOTE_ADDR']; 
+    $pattern  = '/[\r\n]|Content-Type:|Bcc:|Cc:/i';
+    if (preg_match($pattern, $name) || preg_match($pattern, $email) || preg_match($pattern, $subject)) {
+        die("Header injection detected");
+    }
+    $emailIsValid = preg_match('/^[^0-9][A-z0-9._%+-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/', $email);
+    if($name && $email && $emailIsValid && $subject && $message){
+        $subject = "$subjectPrefix $subject";
+        $body = "Name: $name <br /> Email: $email <br /> Message: $message <br /> IP: $ip";
+        $headers  = 'MIME-Version: 1.1' . PHP_EOL;
+        $headers .= 'Content-type: text/html; charset=utf-8' . PHP_EOL;
+        $headers .= "From: $name <$email>" . PHP_EOL;
+        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "Reply-To: $email" . PHP_EOL;
+        $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+        mail($emailTo, $subject, $body, $headers);
+        echo "Your message has been sent!";
+    } else {
+        echo "Your email failed to send.  Please try again.";
+    } 
+}
+?>
+
