63f058bffb8c924f66df9ff83547e48ab5ea889c
MatthewKosloski <matthewkosloski@me>
2015-06-17 16:53:37 -0500

    improved email validation

--- _assets/js/contact.js
+++ _assets/js/contact.js
@@ -1,3 +1,9 @@
+jQuery.validator.addMethod(
+    "hasSpace", function(value, element) {
+        return this.optional(element) || element.value.indexOf(" ") > -1;   
+    }
+);
+
 var self = $("#js-validate");
 var validation = self.validate({
     errorPlacement: function(error, element) {
@@ -6,18 +12,21 @@ var validation = self.validate({
     messages: {
         name: '<span class="pe-7s-close-circle"></span>A full name is required.',
         email: '<span class="pe-7s-close-circle"></span>A valid email is required.',
-        message: '<span class="pe-7s-close-circle"></span>Sending an email without a message?  Weird.'
+        message: '<span class="pe-7s-close-circle"></span>Please enter a message with at least 20 characters.'
     },
     rules: {
         name: {
-            required: true
+            required: true,
+            hasSpace: true,
+            minlength: 4
         },
         email: {
-          required: true,
-          email: true
+            required: true,
+            email: true
         },
         message: {
-          required: true
+            required: true,
+            minlength: 20
         }
     },
     submitHandler: function() {
@@ -36,6 +45,12 @@ var validation = self.validate({
         });
     }
 });
+
+jQuery.extend(jQuery.validator.messages, {
+    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+    minlength: jQuery.validator.format("Please enter at least {0} characters.")
+});
+
 
 
 
